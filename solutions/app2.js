70dab5a628f66a48148613c98c2a4a7cb9f10543
Hamburger Réka <Rka>
2022-09-03 13:56:23 +0200

    js3003

--- solutions/app2.js
+++ solutions/app2.js
@@ -1,5 +1,5 @@
-let cookie = (name, value) => {
-    let expires = new Date(Date.now() + 60 * 60 * 1000);
+export let cookie = (name, value) => {
+export   let expires = new Date(Date.now() + 60 * 60 * 1000);
                document.cookie = `${name}=${value};expires=${expires};path=/`;
              }
              
@@ -7,27 +7,27 @@ let cookie = (name, value) => {
              cookie("uid", "354774631237");
              cookie("ssid", "Bx55OWbHJ0Vt_IGIF");
              
-   let cookieHandler = {
+export  let cookieHandler = {
                getAll() {
-   let keyValuePairs = document.cookie
+export   let keyValuePairs = document.cookie
                    .split("; ")
                    .map((item) => item.split("="));
                  return Object.fromEntries(keyValuePairs);
                },
                toSessionStorage() {
-   let allCookies = this.getAll();
+export   let allCookies = this.getAll();
                  Object.entries(allCookies).forEach(([key, value]) => {
                    sessionStorage.setItem(key, value);
                  });
                },
                flush() {
-   let allCookies = this.getAll();
+export  let allCookies = this.getAll();
                  Object.entries(allCookies).forEach(([entry]) => {
                    document.cookie = `${entry}=; expires=Thu, 01 Jan 1970 00:00:00 UTC;`;
                  });
                },
              };
-             export default cookieHandler;
+             
      
        
    
